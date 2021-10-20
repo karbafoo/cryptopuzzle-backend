30dc5a2558bddf9e2dc61ab65a202b13a314888a
karbafoo <karbafooo@gmail.com>
2021-10-20 13:22:21 +0330

    remote server fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fastify = require('fastify')({
 
 fastify.register(require('./routes'));
 // Run the server!
-fastify.listen(4445, function (err, address) {
+fastify.listen(4445, '0.0.0.0', function (err, address) {
     if (err) {
         fastify.log.error(err);
         process.exit(1);
